93d3c58bfbc82665b82124f60490e510e414831e
Vic Kostrzewski <96265818+costchefski@users.noreply.github.com>
2023-07-17 17:05:38 +0100

    Add files via upload

---
+++ CrashCourse/bucketlist.py
@@ -0,0 +1,13 @@
+places = ['kyoto', 'hebrides', 'colombia', 'estonia', 'korfu']
+
+print(places)
+print(sorted(places))
+print(places)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
