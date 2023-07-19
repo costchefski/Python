8dc4bc2bf8c99dea5830fbdd327d89a069470ad0
Vic Kostrzewski <96265818+costchefski@users.noreply.github.com>
2023-07-19 20:21:14 +0100

    Add files via upload

---
+++ CrashCourse/buffet.py
@@ -0,0 +1,10 @@
+buffet = ('chicken', 'rice', 'salsa', 'beans', 'salad')
+
+print("These are the original choices:")
+for choice in buffet:
+    print(choice)
+
+buffet = ('pizza', 'pierogi', 'kimchi', 'grapefruit', 'falafel')
+print("\nThese are the modified choices:")
+for choice in buffet:
+    print(choice)
