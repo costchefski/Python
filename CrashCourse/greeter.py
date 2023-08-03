a6f0adef6f60a19eec8573b8aa1ef1acb05554bd
Vic Kostrzewski <96265818+costchefski@users.noreply.github.com>
2023-08-03 20:43:23 +0100

    Add files via upload

--- CrashCourse/greeter.py
+++ CrashCourse/greeter.py
@@ -1,5 +1,5 @@
-def greet_user():
+def greet_user(username):
     """Display a simple greeting."""
-    print("Hello!")
+    print(f"Hello, {username.title()}!")
 
-greet_user()
+greet_user('jesse')
